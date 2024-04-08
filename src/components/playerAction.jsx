929eaa8972a9eed34f4b084eee2e4267c5e4ae75
Tyler Cash <148647245+tcash1990@users.noreply.github.com>
2024-04-07 21:17:53 -0500

    Added Player actions
    Completed using hardcoded data

--- src/components/playerAction.jsx
+++ src/components/playerAction.jsx
@@ -3,70 +3,71 @@ import playerActionStyles from "../css/playerAction.module.css"
 const PlayerActions = ({ sender, senderColor, recipient, recipientColor}) => {
     return (
         <div className={playerActionStyles.playerAction}>
-            <span style={{ color: senderColor }}>{sender}</span> hit&nbsp;
+            <span style={{ color: senderColor }}> {sender} </span> &nbsp; hit &nbsp;
             {recipient === 'the base' ? (
-                <span style={{ color: recipientColor }}>{recipient}</span>
+                <span style={{ color: recipientColor }}> {recipient} </span>
             ) : (
-                <span style={{ color: recipientColor }}>{recipient}</span>
+                <span style={{ color: recipientColor }}> {recipient} </span>
             )}
         </div>
     );
 };
 
-const PlayerAction = () => {
+const PlayerAction = ({ actions }) => {
 
-    const [game, setGame] = useState({});
-    const [players, setPlayers] = useState([]);
+    // const [game, setGame] = useState({});
+    // const [players, setPlayers] = useState([]);
 
-    const fetchGame = async (gameId) => {
-        try {
-            const response = await fetch(`/api/games/${gameId}`);
-            if (!response.ok) {
-            throw new Error(`Error fetching game information`);
-            }
-            const data = await response.json();
-            setGame(data.game);
-        } catch (error) {
-            console.error(error);
-        }
-        };
+    // const fetchGame = async (gameId) => {
+    //     try {
+    //         const response = await fetch(`/api/games/${gameId}`);
+    //         if (!response.ok) {
+    //         throw new Error(`Error fetching game information`);
+    //         }
+    //         const data = await response.json();
+    //         setGame(data.game);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    //     };
 
-        const fetchPlayers = async (gameId) => {
-            try {
-                const response = await fetch(`/api/games/${gameId}/players`);
-                if (!response.ok) {
-                    throw new Error(`Error fetching player information`);
-                }
-                const data = await response.json();
-                const playersData = data.players.map(player => ({
-                    codename: player.codename,
-                    action: player.action
-                }));
-                setPlayers(playersData);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+    //     const fetchPlayers = async (gameId) => {
+    //         try {
+    //             const response = await fetch(`/api/games/${gameId}/players`);
+    //             if (!response.ok) {
+    //                 throw new Error(`Error fetching player information`);
+    //             }
+    //             const data = await response.json();
+    //             const playersData = data.players.map(player => ({
+    //                 codename: player.codename,
+    //                 action: player.action
+    //             }));
+    //             setPlayers(playersData);
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     };
     
-        useEffect(() => {
-            let [searchParams] = useSearchParams();
-            const gameId = searchParams.get("gameId");
-            fetchGame(gameId);
-            fetchPlayers(gameId);
-        }, []);
+    //     useEffect(() => {
+    //         let [searchParams] = useSearchParams();
+    //         const gameId = searchParams.get("gameId");
+    //         fetchGame(gameId);
+    //         fetchPlayers(gameId);
+    //     }, []);
     
         return (
-            <div className={playerActionStyles.actionScreen}>
-                {players.map(player => (
-                    <PlayerActions
-                        key={player.codename}
-                        sender={player.codename}
-                        senderColor={game.players.find(p => p.codename === player.codename).color}
-                        recipient={player.action.recipient}
-                        recipientColor={game.players.find(p => p.codename === player.action.recipient).color}
-                    />
-                ))}
-            </div>
-        );
-    }
+                <div className={playerActionStyles.actionScreen}>
+                    {actions.map((action, index) => (  
+                        <PlayerActions
+                            key={index}
+                            sender={action.sender}
+                            senderColor={action.senderColor}
+                            recipient={action.recipient}
+                            recipientColor={action.recipientColor}
+                        />
+                    ))}
+                </div>
+            );
+        };
+        
     export default PlayerAction;
--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -4,10 +4,25 @@ import { useState, useMemo } from "react";
 import gameStyles from "../css/game.module.css";
 
 import PlayerDisplay from "../components/playerDisplay.jsx";
-// import PlayerAction from "../src/components/playerAction.jsx";
+import PlayerAction from "../components/playerAction.jsx";
 import Countdown from "../components/countdown.jsx";
 
 import { getGame } from "../../api/game";
+
+const playerActions = [
+  {sender: "Bob ", senderColor: "red", recipient: "Billy", recipientColor: "green"},
+  {sender: "Billy ", senderColor: "green", recipient: "Bob", recipientColor: "red"},
+  {sender: "Bob ", senderColor: "red", recipient: "base", recipientColor: "green"},
+  {sender: "Bob ", senderColor: "red", recipient: "base", recipientColor: "red"},
+  {sender: "Bob ", senderColor: "red", recipient: "Billy", recipientColor: "green"},
+  {sender: "Billy ", senderColor: "green", recipient: "Bob", recipientColor: "red"},
+  {sender: "Bob ", senderColor: "red", recipient: "base", recipientColor: "green"},
+  {sender: "Bob ", senderColor: "red", recipient: "base", recipientColor: "red"},
+  {sender: "Bob ", senderColor: "red", recipient: "Billy", recipientColor: "green"},
+  {sender: "Billy ", senderColor: "green", recipient: "Bob", recipientColor: "red"},
+  {sender: "Bob ", senderColor: "red", recipient: "base", recipientColor: "green"},
+  {sender: "Bob ", senderColor: "red", recipient: "base", recipientColor: "green"},
+];
 
 const Game = () => {
     const [game, setGame] = useState({});
@@ -34,7 +49,7 @@ const Game = () => {
                 <h1>Game</h1>
             </div>
             {!!game.error || <PlayerDisplay game={game} />}
-            {/* <PlayerAction /> */}
+            {!!game.error || <PlayerAction actions = {playerActions} />}
             <Countdown startTime={30} gameTime={360} />
         </div>
 
