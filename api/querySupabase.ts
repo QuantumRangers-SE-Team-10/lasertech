490bb575ea61fa38ef770446edeebd79e9c0585b
shawnericksen <shawnericksen.github@protonmail.com>
2024-04-21 08:41:01 -0500

    add proxy to supabase in vite config to solve cors errors

--- api/querySupabase.ts
+++ api/querySupabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_DB_URL;
+const supabaseUrl = `${window.location.origin}/supabase`
 const supabaseKey = import.meta.env.VITE_DB_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
